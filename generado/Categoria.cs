39adcaed9ece2f7ae29a891082445f4e7043eb0b
kcontrerasc3-cloud <kcontrerasc3@uteq.edu.ec>
2026-07-25 21:39:53 -0500

    Add files via upload

---
+++ generado/Categoria.cs
@@ -0,0 +1,31 @@
+// File:    Categoria.cs
+// Author:  Game over
+// Created: domingo, 19 de julio de 2026 10:30:27
+// Purpose: Definition of Class Categoria
+
+using System;
+using System.Collections.Generic;
+
+public class Categoria
+{
+   private int idCategoria;
+   private string nombreCategoria;
+   private string slugCategoria;
+   private string iconoCategoria;
+
+   public Categoria crearCategoria()
+   {
+      throw new NotImplementedException();
+   }
+
+   public List<Publicacion> listarPublicacionesCategoria()
+   {
+      throw new NotImplementedException();
+   }
+
+   public Categoria obtenerCategoriaPorSlug()
+   {
+      throw new NotImplementedException();
+   }
+
+}
